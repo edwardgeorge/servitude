7755a9010cac2ebfad6c174f749a88de6217b1c9
Edward George <edwardgeorge@gmail.com>
2009-12-15 23:15:11 +0000

    moved extension into own package
    --HG--
    rename : servitude.pyx => servitude/servitudecoremidi.pyx

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
-    name = 'servitude',
-    ext_modules=[Extension("servitude",
-        ["servitude.pyx"],
+    name='servitude',
+    packages=['servitude'],
+    ext_modules=[Extension("servitude/servitudecoremidi",
+        ["servitude/servitudecoremidi.pyx"],
         extra_link_args = [
             '-framework', 'CoreFoundation',
             '-framework', 'CoreMIDI'
